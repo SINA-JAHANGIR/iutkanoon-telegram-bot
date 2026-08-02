3c21c13ff48bbe3759ea74bb84f55f8a14734754
SINA JAHANGIR <sina.jahangir.013@gmail.com>
2026-08-02 12:09:23 +0330

    feat: add BotConfiguration options model

---
+++ Options/BotConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace IutKanoon.TelegramBot.Options
+{
+    // Represents configuration optoins for the Telegram bot
+    public class BotConfiguration
+    {
+        // Configuration section name in appsettings.json
+        public const string Configuration = "BotConfiguration";
+
+        // Telegram Bot API Token
+        public string BotToken { get; set; } = string.Empty;
+    }
+}
