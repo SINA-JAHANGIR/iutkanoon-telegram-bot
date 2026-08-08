b17b66da5257d7d63446ba33e312e01e40ad9b42
SINA JAHANGIR <sina.jahangir.013@gmail.com>
2026-08-08 09:54:49 +0330

    feat: add UserSession and UserState entities for SQLite database

---
+++ Infrastructure/Data/Entities/UserSession.cs
@@ -0,0 +1,12 @@
+﻿namespace IutKanoon.TelegramBot.Infrastructure.Data.Entities;
+
+public class UserSession
+{
+    public long TelegramId { get; set; }
+    public int WpUserId { get; set; }
+    public string UserEmail { get; set; } = string.Empty;
+    public string UserRole { get; set; } = string.Empty;
+
+    public bool IsAuthenticated { get; set; }
+    public DateTime LinkedAt { get; set; }
+}
---
+++ Infrastructure/Data/Entities/UserState.cs
@@ -0,0 +1,9 @@
+﻿namespace IutKanoon.TelegramBot.Infrastructure.Data.Entities;
+
+public class UserState
+{
+    public long TelegramId { get; set; }
+    public string CurrentState { get; set; } = string.Empty;
+    public string? TempData { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
